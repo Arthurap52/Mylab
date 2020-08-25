176148f123a480d8b80dfecdf775b71728de1eeb
Arthurap52 <josearthurantunespereira@gmail.com>
2020-08-25 15:50:47 -0300

    add particles

--- particles/js/demo.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener('DOMContentLoaded', function () {
-    particleground(document.getElementById('particles'), {
-      dotColor: '#00ff00',
-      lineColor: '#00ff11'
-    });
-    var intro = document.getElementById('intro');
-    intro.style.marginTop = - intro.offsetHeight / 25 + 'px';
-  }, false);
